2322447a222ebc38af564351da5909836ee9cc1f
shubham mishra <shubhammishra4227@gmail.com>
2024-11-08 12:28:30 +0000

    java program to add tweo number

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main 
+{
+	public static void main(String[]sam)
+	{
+		int a,b,d;
+		System.out.println("enter two numbers:");
+		Scanner c = new Scanner(System.in);
+		a=c.nextInt();
+		b=c.nextInt();
+		d=a+b;
+		System.put.println("addition of two numbers:" + d);
+	}
+}
+
